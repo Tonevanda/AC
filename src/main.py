6075d14bad44118fe0bef23c0bc72a274360c521
Tonevanda <jmguellourenco@gmail.com>
2024-11-21 20:52:18 +0000

    organized the code

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-
-awards_players = pd.read_csv("../data/awards_players.csv")
-coaches = pd.read_csv("../data/coaches.csv")
-players_teams = pd.read_csv("../data/players_teams.csv")
-players = pd.read_csv("../data/players.csv")
-series_post = pd.read_csv("../data/teams_post.csv")
-teams = pd.read_csv("../data/teams.csv")
-series_post = pd.read_csv("../data/series_post.csv")
-
-
-pd.set_option('display.max_rows', None)
-
-final_table = pd.DataFrame(columns=['tmID','year','hasWon','points','assists','steals','blocks','rebounds'])
+def loadData():
+    awards_players = pd.read_csv("../data/awards_players.csv")
+    coaches = pd.read_csv("../data/coaches.csv")
+    players_teams = pd.read_csv("../data/players_teams.csv")
+    players = pd.read_csv("../data/players.csv")
+    series_post = pd.read_csv("../data/teams_post.csv")
+    teams = pd.read_csv("../data/teams.csv")
+    series_post = pd.read_csv("../data/series_post.csv")
+    return awards_players, coaches, players_teams, players, series_post, teams, series_post
 
 def players_per_team_averages(players_per_team, year):
+    """
+    This function calculates the average of the players in each team for a given year, based on the past years.
+    If year is 7, it will calculate the averages for the years 1 to 6.
+    """
     players_per_team_year = players_per_team[players_per_team["year"] == year]
 
     players_per_team_prev_years = players_per_team[players_per_team["year"] < year]
@@ -42,20 +42,30 @@ def players_per_team_averages(players_per_team, year):
 
     return players_per_team_year
 
+def main():
+    pd.set_option('display.max_rows', None)
+    awards_players, coaches, players_teams, players, series_post, teams, series_post = loadData()
 
+    final_table = pd.DataFrame(columns=['tmID','year','hasWon','points','assists','steals','blocks','rebounds'])
 
+    teams = teams[['year', 'tmID', 'finals']]
+    players_teams = players_teams[['playerID', 'year', 'tmID', 'points', 'assists', 'rebounds', 'blocks', 'steals']]
+    players_per_team = pd.merge(teams, players_teams, how="inner", left_on=["year", "tmID"], right_on=["year", "tmID"])
 
+    # Calculate the best player stats for each team for each year
+    for year in range(2, 11):
+        final_table = pd.concat([final_table, players_per_team_averages(players_per_team, year)])
 
+    # Rename the columns to Best Player (BP)
+    final_table = final_table.rename(columns={
+        'points': 'BPpoints',
+        'assists': 'BPassists',
+        'steals': 'BPsteals',
+        'blocks': 'BPblocks',
+        'rebounds': 'BPrebounds'
+    })
 
-teams = teams[['year', 'tmID', 'finals']]
-players_teams = players_teams[['playerID', 'year', 'tmID', 'points', 'assists', 'rebounds', 'blocks', 'steals']]
+    final_table.to_csv("res.csv", index=False)
 
-players_per_team = pd.merge(teams, players_teams, how="inner", left_on=["year", "tmID"], right_on=["year", "tmID"])
-
-
-
-for year in range(2, 11):
-    final_table = pd.concat([final_table, players_per_team_averages(players_per_team, year)])
-
-
-final_table.to_csv("res.csv", index=False)
+if __name__ == "__main__":
+    main()
